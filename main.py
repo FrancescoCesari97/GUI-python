71dec9b83cc1a193e05ac6c2d10414c712538a04
FrancescoCesari97 <freedus97@gmail.com>
2024-06-28 12:31:46 +0200

    added buttun

--- main.py
+++ main.py
@@ -10,7 +10,31 @@ from PIL import ImageTk, Image #* for using jpg image and install in the termina
 
 window = Tk() #* instantiate an instance of a window
 
-photo = ImageTk.PhotoImage(Image.open('_7a508162-9427-4cfc-abec-a2e751804a5c.jpg')) #* for using jpg image
+# photo = ImageTk.PhotoImage(Image.open('_7a508162-9427-4cfc-abec-a2e751804a5c.jpg')) #* for using jpg image
+
+photo = PhotoImage(file='pngegg(1).png')
+
+imageButton = PhotoImage(file='pngwing.com(2).png')
+
+
+count = 0
+def click():
+    global count
+    count += 1
+    print(f"you clicked {count} times")
+
+buttun = Button(window,
+                text= "click me",
+                command=click,
+                font=('Arial', 40, 'bold'),
+                image=imageButton,
+                compound='left',
+                fg='blue', bg='#00d1ae',
+                activebackground='blue',
+                activeforeground='#00d1ae',
+                padx=10 )
+buttun.pack()
+buttun.place(x=600, y=0)
 
 label = Label(window,
             text="hello world",
@@ -20,11 +44,14 @@ label = Label(window,
             bd=10, 
             padx=10, 
             pady=20,
-            image=photo, compound='bottom')
+            image=photo,
+            compound='bottom')
+
+
 label.pack()
 label.place(x=0, y=0)
 
-window.geometry("500x500")
+window.geometry("1920x1080")
 window.title("first GUI program")
 
 icon = PhotoImage(file='pngwing.com(2).png')
